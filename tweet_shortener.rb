0426a753c4c54cc94b2ff2ab1306f1fb402aeccc
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-11 23:18:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,8 +16,8 @@ def word_substituter(tweet)
   words = tweet.split(" ")
 
   modified = words.map do |word|
-    # downcase = word.downcase
-    dictionary.keys.include?(word.downcase) ? dictionary[word] : word
+    downcase = word.downcase
+    dictionary.keys.include?(word) ? dictionary[word] : word
   end
 
   return modified.join(" ")
