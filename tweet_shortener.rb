5080377614adfdde0ce8b991e195b1775a277f69
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-11 23:10:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+def word_substituter(tweet)
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+  words = tweet.split(" ")
+
+  modified = words.map do |word|
+    dictionary.keys.include?(word) ? dictionary[word] : word
+  end
+
+  return modified.join(" ")
+end
+
+
+def bulk_tweet_shortener(array_of_tweets)
+  
+end
